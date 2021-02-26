c57568069a0614d1b96528a7c1cadd462f4936a6
Gabriel Okunola <arowosegbe67@gmail.com>
2021-02-26 14:58:40 +0100

    merged with xaxxoo

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,7 @@
-import { AuthProvider } from "@/hooks/useUser";
-import "../styles/index.css";
-import "../styles/app.scss";
-import Toast from 'toastr'
-
-
-Toast.options = {
-  closeButton: true,
-  debug: false,
-  newestOnTop: true,
-  progressBar: true,
-  positionClass: 'toast-top-right',
-  preventDuplicates: false,
-  showDuration: '300',
-  hideDuration: '1000',
-  timeOut: '5000',
-  extendedTimeOut: '1000',
-  showEasing: 'swing',
-  hideEasing: 'linear',
-  showMethod: 'fadeIn',
-  hideMethod: 'fadeOut',
-};
-
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
