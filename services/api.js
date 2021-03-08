aa7e63a0edff6b38010e56b5342269c65b125f2e
Gabriel Okunola <arowosegbe67@gmail.com>
2021-03-08 10:43:44 +0100

    fixing errors with step form

--- services/api.js
+++ services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/api",
-  // baseURL: "https://realhub-api.herokuapp.com/api",
+  // baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/api",
+  baseURL: "https://realhub-api.herokuapp.com/api",
 });
 
 export default api;
