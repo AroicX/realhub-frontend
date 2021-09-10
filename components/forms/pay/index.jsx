23aab7a8c372d28491088fd04ccfd12711c90b57
tobi-bams <teebams49@gmail.com>
2021-09-10 06:14:46 +0100

    update: Linked the payment Page

--- components/forms/pay/index.jsx
+++ components/forms/pay/index.jsx
@@ -4,13 +4,17 @@ import Card from "./steps/card";
 import Success from "./steps/success";
 import Terms from "./steps/terms";
 
-export default function Pay() {
+export default function Pay(props) {
+  const terms = props.currentStep === 1 ? true : false;
+  const breakDown = props.currentStep === 2 ? true : false;
+  const card = props.currentStep === 3 ? true : false;
+  const success = props.currentStep === 4 ? true : false;
   return (
     <>
-      {/* <Terms /> */}
-      {/* <BreakDown /> */}
-      <Card />
-      {/* <Success /> */}
+      {terms && <Terms back={props.back} next={props.next} />}
+      {breakDown && <BreakDown back={props.back} next={props.next} />}
+      {card && <Card back={props.back} next={props.next} />}
+      {success && <Success back={props.back} next={props.next} />}
     </>
   );
 }
