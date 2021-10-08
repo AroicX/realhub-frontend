72a3e46facc86623b3f63e0ed0d29a6d737a87ba
tobi-bams <teebams49@gmail.com>
2021-10-08 11:55:57 +0100

    update: Click outside feature for City dropdown

--- components/dropdowns/citydropdown/citydropdown.jsx
+++ components/dropdowns/citydropdown/citydropdown.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import onCLickOutside from 'react-onclickoutside';
+import onCLickOutside from "react-onclickoutside";
 
 import classes from "./citydropdown.module.css";
 
-export default function CityDropDown(props) {
+function CityDropDown(props) {
   const availableCity = ["abuja", "kano", "minna", "kaduna", "lagos", "ibadan"];
 
   let lowerCaseCity = props.searchCity.toLowerCase();
@@ -16,9 +16,13 @@ export default function CityDropDown(props) {
   }
 
   const textSelectedHandler = (city) => {
-      props.setSearchCity(city);
-      props.setIsCityDropActive(false);
-  }
+    props.setSearchCity(city);
+    props.setIsCityDropActive(false);
+  };
+
+  CityDropDown.handleClickOutside = () => {
+    props.setIsCityDropActive(false);
+  };
   return (
     <div className={classes.CityDropDownContainer}>
       {filteredCity.map((city) => (
@@ -33,3 +37,9 @@ export default function CityDropDown(props) {
     </div>
   );
 }
+
+const clickOutsideConfig = {
+  handleClickOutside: () => CityDropDown.handleClickOutside,
+};
+
+export default onCLickOutside(CityDropDown, clickOutsideConfig);
